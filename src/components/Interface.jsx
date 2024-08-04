3827125b75d33aef39e7b0febeac4c08b8977e24
Maxim E <maermolenko@mail.ru>
2024-08-04 20:28:39 +0300

    regexp changed commit

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -34,7 +34,7 @@ export const Interface = () => {
         if (price.length === 0) {
             return ''
         }
-        if (!(/[0-9]+/.test(price))) {
+        if (!(/[0-9]+$/.test(price))) {
             return ''
         }
 
